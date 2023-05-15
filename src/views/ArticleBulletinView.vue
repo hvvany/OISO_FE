764f795d703f7a7ec00073c45ba8e9785f9b1182
kjunhwan1998 <kjunhwan1998@gmail.com>
2023-05-15 11:53:23 +0900

    feat:공지사항 리스트 기본 틀 제작

--- src/views/ArticleBulletinView.vue
+++ src/views/ArticleBulletinView.vue
@@ -1,16 +1,69 @@
 <template>
   <div>
-    <h1>공지사항 페이지 입니다.</h1>
-
+    <div class="top-back">
+      <div class="top-back__arrow material-symbols-outlined">arrow_back</div>
+      <div>공지사항</div>
+    </div>
+    <ul class="content-list">
+      <li class="content-list__item">
+        <p>공지사항 title</p>
+        <p>2023-05-15</p>
+      </li>
+      <li class="content-list__item">
+        <p>공지사항 title</p>
+        <p>2023-05-15</p>
+      </li>
+      <li class="content-list__item">
+        <p>공지사항 title</p>
+        <p>2023-05-15</p>
+      </li>
+      <li class="content-list__item">
+        <p>공지사항 title</p>
+        <p>2023-05-15</p>
+      </li>
+      <li class="content-list__item">
+        <p>공지사항 title</p>
+        <p>2023-05-15</p>
+      </li>
+      <li class="content-list__item">
+        <p>공지사항 title</p>
+        <p>2023-05-15</p>
+      </li>
+      <li class="content-list__item">
+        <p>공지사항 title</p>
+        <p>2023-05-15</p>
+      </li>
+      <li class="content-list__item">
+        <p>공지사항 title</p>
+        <p>2023-05-15</p>
+      </li>
+      <li class="content-list__item">
+        <p>공지사항 title</p>
+        <p>2023-05-15</p>
+      </li>
+      <li class="content-list__item">
+        <p>공지사항 title</p>
+        <p>2023-05-15</p>
+      </li>
+      <li class="content-list__item">
+        <p>공지사항 title</p>
+        <p>2023-05-15</p>
+      </li>
+      <li class="content-list__item">
+        <p>공지사항 title</p>
+        <p>2023-05-15</p>
+      </li>
+    </ul>
+    <div></div>
     <app-nav></app-nav>
   </div>
 </template>
 
 <script>
-import AppNav from '@/components/layout/AppNav.vue';
+import AppNav from "@/components/layout/AppNav.vue";
 export default {
   name: "BulletinView",
-  components: {AppNav},
+  components: { AppNav },
   data() {
     return {
       message: "",
@@ -21,4 +74,30 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.top-back {
+  position:sticky;
+  top: 0;
+  z-index: 2;
+  background-color: rgb(255, 255, 255);
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  padding: 1rem;
+  font-size: 1.6rem;
+  font-weight: 600;
+  border-bottom:solid 1px gray;
+}
+.top-back__arrow {
+  font-size: 2rem;
+  padding: 0 1rem;
+}
+
+.content-list {
+  padding: 0 1.5rem;
+}
+.content-list__item{
+  padding: 1rem;
+  border-bottom: solid 1px gray;
+}
+</style>
