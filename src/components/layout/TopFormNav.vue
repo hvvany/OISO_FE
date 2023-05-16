4b207f3ca8b9f7fcbe37e5f287502e43ec2069ce
kjunhwan1998 <kjunhwan1998@gmail.com>
2023-05-16 11:11:04 +0900

    feat:add editor library & add form nav

---
+++ src/components/layout/TopFormNav.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="top-back">
+    <a @click="$router.go(-1)" class="top-back__cancel">취소</a>
+    <div class="top-back__title">{{ title }}</div>
+    <a @click="부모함수;" class="top-back__submit">등록</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopFormNav",
+  components: {},
+  props: {
+    title: String,
+  },
+  data() {
+    return {
+      message: "",
+    };
+  },
+  created() {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+.top-back {
+  position: sticky;
+  top: 0;
+  z-index: 2;
+  background-color: rgb(255, 255, 255);
+  width: 100vw;
+  height: 15vw;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  font-weight: 600;
+  border-bottom: solid 1px gray;
+}
+.top-back__cancel {
+  font-size: 1rem;
+  color: rgba(115, 115, 115, 0.682);
+}
+.top-back__title {
+  font-size: 1.3rem;
+}
+.top-back__submit {
+  font-size: 1rem;
+  color: #3485ff;
+}
+</style>
--- src/views/ArticleBoardNewView.vue
+++ src/views/ArticleBoardNewView.vue
@@ -1,32 +1,91 @@
 <template>
   <div>
-    <top-back-nav :title="'게시글'"></top-back-nav>
-    <label>제목</label>
-    <input type="text"/>
-    <label>본문</label>
-    <textarea>
-
-    </textarea>
-    <button>등록</button>
-    <button>취소</button>
+    <top-form-nav :title="'게시글 작성'"></top-form-nav>
+    <input
+      class="input__title"
+      type="text"
+      placeholder="제목을 입력해주세요." />
+    <textarea id="input__content" v-model="content_text"></textarea>
     <app-nav :navmode="'board'"></app-nav>
   </div>
 </template>
 
 <script>
-import TopBackNav from "@/components/layout/TopBackNav.vue";
+import suneditor from "suneditor";
+// import editor plugins
+import image from "suneditor/src/plugins/dialog/link";
+import list from "suneditor/src/plugins/submenu/list";
+import { font, video } from "suneditor/src/plugins";
+import lang from "suneditor/src/lang";
+
+import TopFormNav from "@/components/layout/TopFormNav.vue";
 import AppNav from "@/components/layout/AppNav.vue";
 export default {
   name: "BoardNew",
-  components: { TopBackNav, AppNav },
+  components: { TopFormNav, AppNav },
   data() {
     return {
       message: "",
+      content_text: "",
     };
   },
-  created() {},
+  mounted() {
+    suneditor.create("input__content", {
+      plugins: [font, video, image, list],
+      buttonList: [["font", "video", "image", "list"]],
+      lang: lang.ko,
+    });
+
+    const editor = suneditor.create("example");
+    // Gets a list of images uploaded to the editor
+    /**
+     * {
+     *  element: image element
+     *  src: imgage src
+     *  index: data index
+     *  name: file name
+     *  size: file size
+     *  select: select function
+     *  delete: delete function
+     * }
+     **/
+    editor.getImagesInfo();
+
+    // Gets uploaded files(plugin using fileManager) information list.
+    // image: [img], video: [video, iframe], audio: [audio]
+    // When the argument value is 'image', it is the same function as "getImagesInfo".
+    /**
+     * {
+     *  element: image element
+     *  src: imgage src
+     *  index: data index
+     *  name: file name
+     *  size: file size
+     *  select: select function
+     *  delete: delete function
+     * }
+     * pluginName: Plugin name (image, video, audio)
+     **/
+    // Upload images using image plugin
+    // document.getElementById('example_files_input').files
+    editor.insertImage(FileList);
+  },
   methods: {},
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.input__title {
+  width: 80vw;
+  border-radius: 20px;
+  border-width: 1px;
+  height: 3rem;
+  padding: 0 1rem;
+  margin: 1rem;
+}
+#input__content {
+  width: 99vw;
+  height: 80vh;
+  text-align: start;
+}
+</style>
