e1e956c6756076ea79e005e4ef7f705c64ad0f98
junhwan <kjunhwan1998@gmail.com>
2023-05-17 01:24:32 +0900

    fix:텍스트 스와이퍼 글자 잘림 수정

--- src/components/common/TextSwiper.vue
+++ src/components/common/TextSwiper.vue
@@ -1,19 +1,22 @@
 <template>
-  <div>
-    <swiper
-      :slides-per-view="4.5"
-      :loop="false"
-      @swiper="onSwiper"
-      @slideChange="onSlideChange">
-      <swiper-slide v-for="(value, key, idx) in texts" :key="idx">
-        <!-- texts는 {'관광지':12,...} 처럼 딕셔너리 구조 -->
-        <a v-if="key === now_filter" class="swiper-location--select">{{
-          key
-        }}</a>
-        <a v-else @click="$parent.changeFilter(key)">{{ key }}</a>
-      </swiper-slide>
-    </swiper>
-  </div>
+  <swiper
+    :slides-per-view="4.5"
+    :loop="false"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange">
+    <swiper-slide v-for="(value, key, idx) in texts" :key="idx">
+      <!-- texts는 {'관광지':12,...} 처럼 딕셔너리 구조 -->
+      <a v-if="key === now_filter" class="swiper-location--selected">{{
+        key
+      }}</a>
+      <a
+        v-else
+        @click="$parent.changeFilter(key)"
+        class="swiper-location--unselected"
+        >{{ key }}</a
+      >
+    </swiper-slide>
+  </swiper>
 </template>
 
 <script>
@@ -46,11 +49,18 @@ swiper {
   width: 10rem;
   height: 10rem;
 }
+swiper-slide {
+  font-size: 0.6rem;
+}
 .swiper__slide-img {
   width: 100%;
 }
-.swiper-location--select {
+.swiper-location--selected {
   color: #f24e1e;
   font-weight: 700;
+  height: 1rem;
+}
+.swiper-location--unselected {
+  height: 1rem;
 }
 </style>
