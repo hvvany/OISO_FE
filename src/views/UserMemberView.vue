83a48807d689eedc9028883682a0d9cc0661610a
강다은 <da010228@naver.com>
2023-05-17 19:54:07 +0900

    feat: member design

--- src/views/UserMemberView.vue
+++ src/views/UserMemberView.vue
@@ -1,31 +1,34 @@
 <template>
   <div>
     <top-back-nav :title="'멤버 관리'"></top-back-nav>
-    <div class="member__div" v-for="(member, idx) in memberData" :key="idx">
+    <div class="card__member" v-for="(member, idx) in memberData" :key="idx">
       <input
+        class="text"
         v-if="modifyMode & (member.userId === userId)"
         type="text"
-        v-model="userId" />
-      <h1 v-else>사용자 ID : {{ member.userId }}</h1>
+        v-model="userId"
+        readonly />
+      <div class="text" v-else>사용자 ID : {{ member.userId }}</div>
       <input
+        class="text"
         v-if="modifyMode & (member.userId === userId)"
         type="text"
         v-model="userPwd" />
-      <h1 v-else>사용자 PW : {{ member.userPwd }}</h1>
+      <div class="text" v-else>사용자 PW : {{ member.userPwd }}</div>
       <input
+        class="text"
         v-if="modifyMode & (member.userId === userId)"
         type="email"
         v-model="email" />
-      <h1 v-else>
-        사용자 E-MAIL : {{ member.emailId }} @ {{ member.emailDomain }}
-      </h1>
-      <button v-if="!modifyMode" @click="changeModify(member)">수정</button>
-      <div v-else>
-        <button @click="$router.go()">취소</button>
-        <button @click="modifyMember()">수정 완료</button>
+      <div class="text" v-else>
+        사용자 E-MAIL : {{ member.emailId }}@{{ member.emailDomain }}
       </div>
+      <button v-if="!modifyMode" @click="changeModify(member)">수정</button>
+      <span v-else>
+        <button @click="$router.go()">취소</button>
+        <button @click="modifyMember()">수정</button>
+      </span>
       <button @click="deleteMember(member)">삭제</button>
-      <hr />
     </div>
     <app-nav></app-nav>
   </div>
@@ -99,13 +102,32 @@ export default {
 </script>
 
 <style scoped>
+.card__member {
+  background-color: white;
+  border-radius: 8px;
+  margin: 2vw;
+  padding: 2vw;
+}
+
+input {
+  width: 80%;
+  border: 1px solid #989898;
+  border-radius: 8px;
+  padding: 10px 12px;
+  font-size: 14px;
+}
+
+.text {
+  margin: 1vw;
+}
+
 button {
   width: 20%;
   border: 1px solid #989898;
   background-color: white;
   border-radius: 8px;
-  padding: 10px 12px;
-  margin: 0px 10px;
+  padding: 6px 12px;
+  margin: 6px 10px;
   font-size: 12px;
 }
 </style>
