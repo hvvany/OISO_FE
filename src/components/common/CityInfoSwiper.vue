250f177016da1ed87384cb703f9e9c8b353c9663
daeun <da010228@naver.com>
2023-05-17 11:30:43 +0900

    feat: city info swiper 에 weater 기본 정보

--- src/components/common/CityInfoSwiper.vue
+++ src/components/common/CityInfoSwiper.vue
@@ -7,25 +7,17 @@
       @slideChange="onSlideChange">
       <swiper-slide
         class="swiper__slide-img"
-        v-for="(val, key, idx) in weather"
+        v-for="(value, key, idx) in weather"
         :key="idx">
         <div class="cards__card">
-          <!-- <div class="weather_text">
-            <div>지금 {{ val[0] }}의</div>
-            <div>온도는 {{ val[1] }}℃</div>
-            <div style="font-size: 1rem">
-              최저 {{ val[2] }}℃, 최고 {{ val[3] }}℃
-            </div>
-          </div> -->
-          <!-- 이거는 db에서 해서 해당 사진을 들고오거나/ 안 바뀌니까 img에 넣어둬도 될 것 같고 -->
-          <!-- <div>하늘은 {{ val[4] }} + {{ val[5] }}</div> -->
-
-          <!-- 아니면 밑에 버전으로 해서 깔끔하게 이모티콘으로 지금 하늘 표시해도 될 것 같고? -->
           <div class="weather_text">
-            <div>지금 {{ val[0] }}의</div>
-            <div>온도는 {{ val[1] }}℃</div>
+            <div>지금 {{ value["LOC"] }} 의</div>
+            <div>온도는 {{ value["TMP"] }}℃</div>
             <div style="font-size: 1rem">
-              최저 {{ val[2] }}℃, 최고 {{ val[3] }}℃
+              최저 {{ value["SKY"] }}℃, 최고 {{ value["TMX"] }}℃
+            </div>
+            <div style="font-size: 1rem">
+              하늘 {{ value["SKY"] }}, 비 {{ value["PTY"] }}
             </div>
           </div>
           <div class="weather_img">
@@ -41,9 +33,9 @@
 
 <script>
 import { Swiper, SwiperSlide } from "swiper-vue2";
-
-// Import Swiper styles
 import "swiper/swiper-bundle.css";
+
+import http from "@/util/http-common.js";
 
 export default {
   props: {
@@ -55,46 +47,32 @@ export default {
   },
   data() {
     return {
-      weather: {
-        감천문화마을: [
-          "서울",
-          19, //1시간 기온
-          27, //최저 기온
-          14, //최고 기온
-          1, //하늘 상태
-          0, //강수 상태
-          "https://media.istockphoto.com/id/1007768414/ko/%EC%82%AC%EC%A7%84/%EB%B0%9D%EC%9D%80-%ED%83%9C%EC%96%91%EA%B3%BC-%EA%B5%AC%EB%A6%84%EA%B3%BC-%ED%91%B8%EB%A5%B8-%ED%95%98%EB%8A%98.jpg?b=1&s=170667a&w=0&k=20&c=6NhhjASHzk5ENQz8hUUL8F1Nh0HTcN1611VcyZDhr2g=",
-        ],
-        해운대: [
-          "부산",
-          19,
-          27,
-          14,
-          1,
-          0,
-          "https://www.visitbusan.net/uploadImgs/files/cntnts/20191229153530671_oen",
-        ],
-        광안리: [
-          "울산",
-          19,
-          27,
-          14,
-          1,
-          0,
-          "https://images.unsplash.com/photo-1558486012-817176f84c6d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d2VhdGhlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1200&q=60",
-        ],
-        애월: [
-          "제주도",
-          19,
-          27,
-          14,
-          1,
-          0,
-          "https://images.unsplash.com/photo-1534794048419-48e110dca88e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHdlYXRoZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=1200&q=60",
-        ],
+      weather: { 1: [] },
+      sidos: {
+        서울: { sido_code: 1, nx: 60, ny: 127 },
+        인천: { sido_code: 2, nx: 55, ny: 124 },
+        대전: { sido_code: 3, nx: 67, ny: 100 },
+        대구: { sido_code: 4, nx: 89, ny: 90 },
+        광주: { sido_code: 5, nx: 58, ny: 74 },
+        부산: { sido_code: 6, nx: 98, ny: 76 },
+        울산: { sido_code: 7, nx: 102, ny: 84 },
+        세종: { sido_code: 8, nx: 66, ny: 103 },
+        경기도: { sido_code: 31, nx: 60, ny: 120 },
+        강원도: { sido_code: 32, nx: 73, ny: 134 },
+        충청북도: { sido_code: 33, nx: 69, ny: 107 },
+        충청남도: { sido_code: 34, nx: 68, ny: 100 },
+        경상북도: { sido_code: 35, nx: 89, ny: 91 },
+        경상남도: { sido_code: 36, nx: 91, ny: 77 },
+        전라북도: { sido_code: 37, nx: 63, ny: 89 },
+        전라남도: { sido_code: 38, nx: 51, ny: 67 },
+        제주도: { sido_code: 39, nx: 52, ny: 38 },
       },
     };
   },
+  created() {
+    this.getWeatherInfo();
+  },
+  computed: {},
   methods: {
     onSwiper(swiper) {
       console.log(swiper);
@@ -102,33 +80,79 @@ export default {
     onSlideChange() {
       console.log("slide change");
     },
+    getWeatherInfo() {
+      //이걸 계획만큼 반복해야 한다..!
+      const request_url =
+        "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=" +
+        process.env.VUE_APP_WEATHER_KEY +
+        "&pageNo=1&numOfRows=14" +
+        "&dataType=JSON&base_date=20230516&base_time=0500" +
+        "&nx=" +
+        "94" +
+        "&ny=" +
+        "74";
+
+      http.get(request_url).then((response) => {
+        let infos = {
+          LOC: "서울",
+          TMP: 0,
+          TMN: 0,
+          TMX: 0,
+          SKY: 0,
+          PTY: 0,
+        };
+        for (let info of response.data.response.body.items.item) {
+          // console.log(info);
+          if (info.category === "TMP") {
+            infos.TMP = info.fcstValue;
+          } else if (info.category === "TMN") {
+            infos.TMN = info.fcstValue;
+          } else if (info.category === "TMX") {
+            infos.TMX = info.fcstValue;
+          } else if (info.category === "SKY") {
+            infos.SKY = info.fcstValue;
+          } else if (info.category === "PTY") {
+            infos.PTY = info.fcstValue;
+          }
+        }
+        this.weather[1] = infos;
+        console.log(this.weather);
+      });
+      // this.getWeather();
+    },
+    // getWeather() {
+
+    // },
   },
 };
 </script>
 <style scoped>
 .swiper__slide-img {
   width: 100rem;
-  height: 10rem;
+  height: 8rem;
   color: aliceblue;
   text-align: left;
-  line-height: 2.4rem;
-  font-size: 1.6rem;
+  line-height: 2rem;
+  font-size: 1.4rem;
 }
 .cards__card {
   width: 100vw;
-  height: 9.6rem;
+  height: 8rem;
   background-color: gray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .weather_text {
   float: left;
-  margin-left: 1rem;
 }
 .weather_img {
+  margin-left: 1rem;
   float: right;
 }
 
 .img {
-  width: 9rem;
-  height: 9rem;
+  width: 6rem;
+  height: 6rem;
 }
 </style>
